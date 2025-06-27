9131b83c786c335a7ef8ba6a94aea2702cf6ebe8
Eric <as2229181@gmail.com>
2025-06-27 20:48:06 +0800

    feat: udpate models

--- src/models/split.py
+++ src/models/split.py
@@ -13,7 +13,7 @@ class Spilt(db.Model):
     payment_uuid = db.Column(UUID(as_uuid=True), db.ForeignKey("payments.uuid"))
     debtor_uuid = db.Column(UUID(as_uuid=True), db.ForeignKey("users.uuid"))
 
-    amount = db.Column(db.Column(db.Integer, nullable=False))
+    amount = db.Column(db.Integer, nullable=False)
 
     deleted_datetime = db.Column(db.DateTime)
     updated_datetime = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
