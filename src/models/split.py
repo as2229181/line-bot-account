52cb7b3a634df95e836af6408713c8f2c4bfc7fe
Eric <as2229181@gmail.com>
2025-06-27 19:50:58 +0800

    docs: add models

---
+++ src/models/split.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from sqlalchemy.dialects.postgresql import UUID
+
+from database import db
+
+
+class Spilt(db.Model):
+    __tablename__ = 'spilts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(UUID(as_uuid=True), unique=True, nullable=False)
+    payment_uuid = db.Column(UUID(as_uuid=True), db.ForeignKey("payments.uuid"))
+    debtor_uuid = db.Column(UUID(as_uuid=True), db.ForeignKey("users.uuid"))
+
+    amount = db.Column(db.Column(db.Integer, nullable=False))
+
+    deleted_datetime = db.Column(db.DateTime)
+    updated_datetime = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+    created_datetime = db.Column(db.DateTime, default=datetime.now)
---
+++ src/models/user.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from sqlalchemy.dialects.postgresql import UUID
+
+from database import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(UUID(as_uuid=True), unique=True, nullable=False)
+
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.Text)
+    email = db.Column(db.String(80), unique=True)
+
+    updated_datetime = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+    created_datetime = db.Column(db.DateTime, default=datetime.now)
